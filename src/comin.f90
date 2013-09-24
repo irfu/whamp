d6efcfb9c7e2ea3959009930d5185190f765a9b8
Andris Vaivads <andris.vaivads@gmail.com>
2013-09-24 22:00:49 +0200

    clean up comin comcout

--- src/comin.f90
+++ src/comin.f90
@@ -1,30 +1,30 @@
-!**************************************************************
-!
-! File comin.h 
+module comin
+implicit none
+!integer, parameter,private :: d2p=kind(1.0d0)
+integer, parameter,private :: d2p=8
 
- module comin
-        implicit none
-! find the kind of a high precision variable, by finding 
-! the kind of 1.0d0
-        integer, parameter,private :: d2p=kind(1.0d0)
+integer :: JMA             ! the number of last plasma species with non-zero density
 
-!common block plasma
-        integer :: JMA
-        real(kind=d2p) :: BVEC(3),DBVDR(3,4),DBDR(4),DNDR(4,10),DTDR(4,10),&
-        & VDRIFT(10),ZIGN 
-        complex(kind=d2p) :: GE(6,4) 
+real(kind=d2p) :: PP(10)   ! ??
+real(kind=d2p) :: ZZ(10)   ! ??
+real(kind=d2p) :: AA(10,2) ! alpha parameters in distribution function
+real(kind=d2p) :: DD(10)   !
+real(kind=d2p) :: ASS(10)  ! mass of components
+real(kind=d2p) :: VD(10)   ! drift velocity 
+real(kind=d2p) :: DN(10)   ! density of each plasma component cm^-3
+real(kind=d2p) :: TA(10)   ! temperature in keV
+real(kind=d2p) :: CV       ! speed of light / thermal velocity of 1st species
+real(kind=d2p) :: PM(3)    ! array (pmin,pmax,pstep)
+real(kind=d2p) :: ZM(3)    ! array (zmin,zmax,zstep)
+real(kind=d2p) :: XOI      ! initialization frequency (real part)
+real(kind=d2p) :: XC       ! XC  - gyrofrequency (kHZ)
+real(kind=d2p) :: PZL      ! option L, =0 linear input, =1 logarythmic input of P and Z
 
-!common block XPZ
-        real(kind=d2p) :: PP(10),ZZ(10),AA(10,2),DD(10),ASS(10),VD(10),&
-       & DN(10),TA(10),CV,PM(3),ZM(3),XOI,XC,PZL 
-        complex(kind=d2p) :: XX(10),XP(10) 
+complex(kind=d2p) :: XX(10)
+complex(kind=d2p) :: XP(10) 
 
- end module comin
+end module comin
 
-! CV  - speed of light / thermal velocity of 1st species
-! JMA - the number of last plasma species with non-zero density
-! PM  - array (pmin,pmax,pstep)
-! PZL - option L, =0 linear input, =1 logarythmic input of P and Z
-! ZM  - arra (zmin,zmax,zstep)
-! XC  - gyrofrequency (kHZ)
-! XOI - initialization frequency (real part)
+
+
+
